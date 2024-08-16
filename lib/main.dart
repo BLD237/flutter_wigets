669dbcb468388b7e266c203213da8ef0da9793e5
belmond <muforbelmond20@gmail.com>
2024-08-16 14:23:43 +0100

    modified:   lib/main.dart 	deleted:    lib/pages/home.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:childwiget/pages/home.dart';
 
 void main() => runApp(const MyApp());
 
@@ -14,5 +13,80 @@ class MyApp extends StatelessWidget {
     );
   }
 }
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  String name = '';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.blue,
+        title: const Text('Custom Widgets'),
+      ),
+      body: Center(
+        
+        child: Column(          
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            NameInputWidget(
+              onNameChanged: (newName) {
+                setState(() {
+                  name = newName;
+                });
+              },
+            ),
+            const SizedBox(height: 20),
+            Text('Your name is: $name'),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+// 1. Child Widget: NameInputWidget
+class NameInputWidget extends StatefulWidget {
+  final Function(String) onNameChanged;
+
+  const NameInputWidget({super.key, required this.onNameChanged});
+
+  @override
+  _NameInputWidgetState createState() => _NameInputWidgetState();
+}
+
+class _NameInputWidgetState extends State<NameInputWidget> {
+  final _nameController = TextEditingController();
+
+  @override
+  void dispose() {
+    _nameController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+      controller: _nameController,
+      cursorColor: Colors.blue,
+      decoration: const InputDecoration(
+        labelText: 'Enter your name',
+        border: OutlineInputBorder()
+      ),
+      onChanged: (text) {
+        widget.onNameChanged(text);
+      },
+    );
+  }
+}
+
+
 //day 12 0f 30 days coding challenge
 
