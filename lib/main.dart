3c60820662cd91ebb0c51d36ebe0e32e216675b4
belmond <muforbelmond20@gmail.com>
2024-08-16 14:00:25 +0100

    modified:   lib/main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:childwiget/pages/home.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       title: 'Custom Wigets',
       home: MyHomePage(),
     );
